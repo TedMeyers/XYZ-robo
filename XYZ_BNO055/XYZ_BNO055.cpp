be81673a815d0d42a1a60b126b30d17424213d83
TedMeyers <Ted.Meyers@gmail.com>
2016-04-06 22:52:45 -0600

    Update XYZ_BNO055.cpp

--- XYZ_BNO055/XYZ_BNO055.cpp
+++ XYZ_BNO055/XYZ_BNO055.cpp
@@ -249,7 +249,7 @@ void XYZ_BNO055::readBytes(uint8_t address, uint8_t subAddress, uint8_t count, u
 	// Wire.write(subAddress);            // Put slave register address in Tx buffer
 	// Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
 	// uint8_t i = 0;
-	//  Wire.requestFrom(address, (size_t) count);  // Read bytes from slave register address 
+	// Wire.requestFrom(address, (size_t) count);  // Read bytes from slave register address 
 	// while (Wire.available()) {
 	//    dest[i++] = Wire.read(); 
 	//  }         // Put read results in the Rx buffer
